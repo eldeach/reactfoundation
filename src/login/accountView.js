eb5e9e7c9b62e684f1bb45176693683bc4c9d614
Donhyeong Lee <dhm_w@icloud.com>
2022-08-09 23:33:07 +0900

    데이터 읽어와 테이블로 뿌려주는 기능 개발중

--- src/login/accountView.js
+++ src/login/accountView.js
@@ -1,9 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Table, FloatingLabel, Button,Container,Row,Col  } from 'react-bootstrap';
+import axios from 'axios';
 
 function AccountView(){
-    let [tableHeight,setTableHeight] = useState(700);
+    let [qryAccount,setQryAccount] = useState("");
+    let [listData,setListData] = useState([])
 
+  useEffect(() => {
+
+    let data = {
+        user_account : qryAccount
+   };
+
+   axios({
+       method:"get",
+       url:"/getaccountview",
+       params:{user_account : qryAccount},
+       headers:{
+           'Content-Type':'application/json'
+       }
+
+   }).then((res)=>{
+       console.log(res);
+       setListData(res.data)
+   })
+
+    // async function getResults() {
+    //     let body={
+    //         id:"a"
+    //     }
+
+    //     const results = await axios('/accountview', body).then((v)=>{
+    //         console.log(v.data)
+    //       }).catch((err)=>console.log(err))
+    //   }
+
+  },[]);
     return(
         <div>
             <div className="tblheader">
@@ -15,138 +47,47 @@ function AccountView(){
                 <h1>aaa</h1>
                 <h1>aaa</h1>
             </div>
-            <div className="tbl" style={{height:`${tableHeight}px`}}>
-                <Table size="sm" striped bordered hover>
-                    <thead style ={{backgroundColor:"white",position:"fixed"}}>
-                        <tr className="tableFixHead">
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>a</td>
-                            <td>b</td>
-                            <td>c</td>
-                        </tr>
-                        <tr>
-                        <td>1</td>
-                            <td>a</td>
-                            <td>b</td>
-                            <td>c</td>
-                        </tr>
-                        <tr>
-                        <td>1</td>
-                            <td>a</td>
-                            <td>b</td>
-                            <td>c</td>
-                        </tr>
-                        <tr>
-                        <td>1</td>
-                            <td>a</td>
-                            <td>b</td>
-                            <td>c</td>
-                        </tr>
-                        <tr>
-                        <td>1</td>
-                            <td>a</td>
-                            <td>b</td>
-                            <td>c</td>
-                        </tr>
-                        <tr>
-                        <td>1</td>
-                            <td>a</td>
-                            <td>b</td>
-                            <td>c</td>
-                        </tr>
-                        <tr>
-                        <td>1</td>
-                            <td>a</td>
-                            <td>b</td>
-                            <td>c</td>
-                        </tr>
-                        <tr>
-                        <td>1</td>
-                            <td>a</td>
-                            <td>b</td>
-                            <td>c</td>
-                        </tr>
-                        <tr>
-                        <td>1</td>
-                            <td>a</td>
-                            <td>b</td>
-                            <td>c</td>
-                        </tr>
-                        <tr>
-                        <td>1</td>
-                            <td>a</td>
-                            <td>b</td>
-                            <td>c</td>
-                        </tr>
-                        <tr>
-                        <td>1</td>
-                            <td>a</td>
-                            <td>b</td>
-                            <td>c</td>
-                        </tr>
-                        <tr>
-                        <td>1</td>
-                            <td>a</td>
-                            <td>b</td>
-                            <td>c</td>
-                        </tr>
-                        <tr>
-                        <td>1</td>
-                            <td>a</td>
-                            <td>b</td>
-                            <td>c</td>
-                        </tr>
-                        <tr>
-                        <td>1</td>
-                            <td>a</td>
-                            <td>b</td>
-                            <td>c</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                        </tr>
-                    </tbody>
-                </Table>
+            <div className='tbl'>
+            {
+                listData.length>0?<Listup listData={listData}/>:<div></div>
+            }
             </div>
             <div className='tblfooter'>
                 <h1>table page footer</h1>
             </div>
         </div>
+    )
+
+}
+
+function Listup(props){
+    return(
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                    {
+                         Object.keys(props.listData[0]).map((columName,i)=>{
+                            return <th key={i}>{columName}</th>   
+                        })
+                    }
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        props.listData.map((oneRow,i)=>{
+                            return(
+                            <tr>
+                                {
+                                    Object.values(oneRow).map((oneValue,i)=>{
+                                        return <td>{typeof(oneValue)=='string' || typeof(oneValue)=='number' ? oneValue : ''}</td> 
+                                    })
+                                }
+                            </tr>
+                            )
+                        })           
+                    }
+                </tbody>
+            </Table>
     )
 
 }
